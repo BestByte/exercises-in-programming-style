290d4d5e4079895ad121e8788988a3ebe1611aff
BestByte <ilvxna@hotmail.com>
2019-09-05 22:35:45 +0800

    up

--- 11-things/tf-11-thing.py
+++ 11-things/tf-11-thing.py
@@ -2,4 +2,11 @@
 import sys,re,operator,string
 
 class DataStorage():
-    def __init__(self,)
+    '''
+
+    '''
+    def __init__(self,path_to_file:string):
+        with open(path_to_file) as f:
+            self._data=f.read()
+
+        pattern=re.compile('')
