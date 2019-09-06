3ee7c6d7e8a42ae284757e526525b4e965601749
BestByte <ilvxna@hotmail.com>
2019-09-06 20:19:02 +0800

    add datastore

--- 11-things/tf-11-one.py
+++ 11-things/tf-11-one.py
@@ -1,2 +1,22 @@
 import sys,re,operator,string
 
+class DataStore():
+    '''
+
+    '''
+    def __init__(self,path_to_file):
+        with open(path_to_file) as f:
+            self._data=f.read()
+
+        pattern=re.compile('[\W_]+')
+        self._data=pattern.sub('',self._data).lower()
+
+
+    def words(self):
+        '''
+        '''
+        return self._data.split()
+
+
+    def info(self):
+        return super(DataStore,self)
