0967671fd52de4fbe9df376407d29bb5376d5bbf
BestByte <ilvxna@hotmail.com>
2019-09-06 22:48:08 +0800

    up

--- 11-things/tf-11-one.py
+++ 11-things/tf-11-one.py
@@ -6,7 +6,7 @@ class InfoAbstaract():
     def info(self):
         return self.__class__.__name__
 
-class DataStore():
+class DataStore(InfoAbstaract):
     '''
 
     '''
@@ -26,4 +26,19 @@ class DataStore():
 
     def info(self):
         return super(DataStore,self).info()+"My major data is a "+self._data.__class__.__name__
+
+class StopWordManager(InfoAbstaract):
+
+    def __init__(self):
+        with open("../stop_words.txt") as f:
+            self._stop_words=f.read().split(',')
+
+            self._stop_words.extend(list(string.ascii_letters))
+
+    def is_stop_word(self,word):
+        return word in self._stop_words
+
+
+    def info(self):
+        return super(StopWordManager,self).info()+" my major is a"+self._stop_words.__class__.__name__
 
