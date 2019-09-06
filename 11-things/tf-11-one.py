1ee46d54db252c212a789ce8697021ad68ba3f37
BestByte <ilvxna@hotmail.com>
2019-09-06 22:55:59 +0800

    finsih

--- 11-things/tf-11-one.py
+++ 11-things/tf-11-one.py
@@ -62,4 +62,27 @@ class WordFreqManager(InfoAbstaract):
     def info(self):
         return super(WordFreqManager,self).info()+"my magor is "+self.__word_freqs.__class__.__name__
 
+
+class WordFrequencyController(TFExercise):
+    def __init__(self, path_to_file):
+        self._storage_manager = DataStore(path_to_file)
+        self._stop_word_manager = StopWordManager()
+        self._word_freq_manager = WordFreqManager()
+
+    def run(self):
+        for w in self._storage_manager.words():
+            if not self._stop_word_manager.is_stop_word(w):
+                self._word_freq_manager.increment_count(w)
+
+        word_freqs = self._word_freq_manager.sorted()
+        for (w, c) in word_freqs[0:25]:
+            print(w, ' - ', c)
+
+
+#
+# The main function
+#
+WordFrequencyController(sys.argv[1]).run()
+
+
      
